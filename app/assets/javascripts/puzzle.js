42465b8eed86eb7d31e5da22a59c9f134b807131
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-02-20 13:54:24 +0900

    puzzle-image-hide

--- app/assets/javascripts/puzzle.js
+++ app/assets/javascripts/puzzle.js
@@ -7,10 +7,49 @@
     
     
     
-    ctx.drawImage(image,
-      0, 0, 180, 180,
-      0, 0, 180, 180).setAttribute(id,1_1);
-    // ctx.drawImage(image)
+    const a = ctx.drawImage(image,
+      0, 0, 540, 540,
+      0, 0, 540, 540);
+
+    const img1_1 = ctx.getImageData(120, 0, 180, 180);
+    ctx.strokeStyle = "rgb(0,0,0)";
+    ctx.beginPath();
+    ctx.moveTo(180,0);
+    ctx.lineTo(180,540);
+    ctx.stroke();
+
+
+    ctx.fillStyle = "rgb(0, 0, 255)";
+    ctx.fillRect(  0,  0,180,180); // 1_1
+    ctx.fillRect(180,  0,180,180); // 1_2
+    ctx.fillRect(360,  0,180,180); // 1_3
+    ctx.fillRect(  0,180,180,180); // 2_1
+    ctx.fillRect(180,180,180,180); // 2_2
+    ctx.fillRect(360,180,180,180); // 2_3
+    ctx.fillRect(  0,360,180,180); // 3_1
+    ctx.fillRect(180,360,180,180); // 3_2
+    ctx.fillRect(360,360,180,180); // 3_3
+
+    ctx.strokeStyle = "rgb(255,255,255,0.7)";
+    ctx.beginPath();
+    ctx.moveTo(180,0);
+    ctx.lineTo(180,540);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(360,0);
+    ctx.lineTo(360,540);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0,180);
+    ctx.lineTo(540,180);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0,360);
+    ctx.lineTo(540,360);
+    ctx.stroke();
   };
     
   image.src = "/assets/puzzle1.jpg";
