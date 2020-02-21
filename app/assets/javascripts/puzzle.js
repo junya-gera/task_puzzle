68ee085e6e8557ae4bc1f41db1fd090e8d3f2554
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-02-21 16:10:43 +0900

    canvas

--- app/assets/javascripts/puzzle.js
+++ app/assets/javascripts/puzzle.js
@@ -6,30 +6,56 @@ $(function() {
     const canvas = document.getElementById("stage");
     const ctx = canvas.getContext("2d");
     
-    
+
     
     const a = ctx.drawImage(image,
       0, 0, 540, 540,
       0, 0, 540, 540);
 
-    const img1_1 = ctx.getImageData(120, 0, 180, 180);
-    ctx.strokeStyle = "rgb(0,0,0)";
-    ctx.beginPath();
-    ctx.moveTo(180,0);
-    ctx.lineTo(180,540);
-    ctx.stroke();
+    const top_left = document.getElementById("top-left");
+    const TLCtx = top_left.getContext("2d");
+    TLCtx.fillStyle = "rgb(0, 0, 255)";
+    TLCtx.fillRect(  0,  0,180,180); // 1_1
 
+    const top_center = document.getElementById("top-center");
+    const TCCtx = top_center.getContext("2d");
+    TCCtx.fillStyle = "rgb(0, 0, 255)";
+    TCCtx.fillRect(0,  0,180,180); // 1_2
 
-    ctx.fillStyle = "rgb(0, 0, 255)";
-    ctx.fillRect(  0,  0,180,180); // 1_1
-    ctx.fillRect(180,  0,180,180); // 1_2
-    ctx.fillRect(360,  0,180,180); // 1_3
-    ctx.fillRect(  0,180,180,180); // 2_1
-    ctx.fillRect(180,180,180,180); // 2_2
-    ctx.fillRect(360,180,180,180); // 2_3
-    ctx.fillRect(  0,360,180,180); // 3_1
-    ctx.fillRect(180,360,180,180); // 3_2
-    ctx.fillRect(360,360,180,180); // 3_3
+    const top_right = document.getElementById("top-right");
+    const TRCtx = top_right.getContext("2d");
+    TRCtx.fillStyle = "rgb(0, 0, 255)";
+    TRCtx.fillRect(0,  0,180,180); // 1_3
+
+    const center_left = document.getElementById("center-left");
+    const CLCtx = center_left.getContext("2d");
+    CLCtx.fillStyle = "rgb(0, 0, 255)";
+    CLCtx.fillRect(  0,0,180,180); // 2_1
+
+    const center_center = document.getElementById("center-center");
+    const CCCtx = center_center.getContext("2d");
+    CCCtx.fillStyle = "rgb(0, 0, 255)";
+    CCCtx.fillRect(0,0,180,180); // 2_2
+
+    const center_right = document.getElementById("center-right");
+    const CRCtx = center_right.getContext("2d");
+    CRCtx.fillStyle = "rgb(0, 0, 255)";
+    CRCtx.fillRect(0,0,180,180); // 2_3
+
+    const bottom_left = document.getElementById("bottom-left");
+    const BLCtx = bottom_left.getContext("2d");
+    BLCtx.fillStyle = "rgb(0, 0, 255)";
+    BLCtx.fillRect(  0,0,180,180); // 3_1
+
+    const bottom_center = document.getElementById("bottom-center");
+    const BCCtx = bottom_center.getContext("2d");
+    BCCtx.fillStyle = "rgb(0, 0, 255)";
+    BCCtx.fillRect(0,0,180,180); // 3_2
+
+    const bottom_right = document.getElementById("bottom-right");
+    const BRCtx = bottom_right.getContext("2d");
+    BRCtx.fillStyle = "rgb(0, 0, 255)";
+    BRCtx.fillRect(0,0,180,180); // 3_3
 
     ctx.strokeStyle = "rgb(255,255,255,0.7)";
     ctx.beginPath();
