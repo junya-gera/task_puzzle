5920656a1903825400042e01c02153a7ccd79c07
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-02-21 14:45:29 +0900

    add-hide-canvas

--- app/assets/javascripts/puzzle.js
+++ app/assets/javascripts/puzzle.js
@@ -1,36 +1,38 @@
 "use strict";
-{
-  const image = new Image();
-  image.onload = function(e){
-    const canvas = document.getElementById("stage");
-    const ctx = canvas.getContext("2d");
-    
-    
-    
-    const a = ctx.drawImage(image,
+window.onload = function(){
+  const image = new Image();  //インスタンス化
+  image.src = "/assets/puzzle1.jpg";  //画像ファイルの読み込み
+  const canvas = document.getElementById("stage");
+  const ctx = canvas.getContext("2d");
+
+  image.addEventListener('load', function(e){
+  
+    ctx.drawImage(image,
       0, 0, 540, 540,
       0, 0, 540, 540);
 
-    const img1_1 = ctx.getImageData(120, 0, 180, 180);
-    ctx.strokeStyle = "rgb(0,0,0)";
-    ctx.beginPath();
-    ctx.moveTo(180,0);
-    ctx.lineTo(180,540);
-    ctx.stroke();
+  });
+
+    const bottomRight = document.getElementById("bottom_right");
+    console.log(bottomRight);
+    const BRCtx = bottomRight.getContext("2d");
+    BRCtx.fillStyle = "rgb(0, 0, 255,1.0)";   //9つの青マス
+
+    BRCtx.fillRect(360,360,180,180); // 3_3
 
 
-    ctx.fillStyle = "rgb(0, 0, 255)";
-    ctx.fillRect(  0,  0,180,180); // 1_1
-    ctx.fillRect(180,  0,180,180); // 1_2
-    ctx.fillRect(360,  0,180,180); // 1_3
-    ctx.fillRect(  0,180,180,180); // 2_1
-    ctx.fillRect(180,180,180,180); // 2_2
-    ctx.fillRect(360,180,180,180); // 2_3
-    ctx.fillRect(  0,360,180,180); // 3_1
-    ctx.fillRect(180,360,180,180); // 3_2
-    ctx.fillRect(360,360,180,180); // 3_3
 
-    ctx.strokeStyle = "rgb(255,255,255,0.7)";
+    // ctx.fillRect(  0,  0,180,180); // 1_1
+    // ctx.fillRect(180,  0,180,180); // 1_2
+    // ctx.fillRect(360,  0,180,180); // 1_3
+    // ctx.fillRect(  0,180,180,180); // 2_1
+    // ctx.fillRect(180,180,180,180); // 2_2
+    // ctx.fillRect(360,180,180,180); // 2_3
+    // ctx.fillRect(  0,360,180,180); // 3_1
+    // ctx.fillRect(180,360,180,180); // 3_2
+    // ctx.fillRect(360,360,180,180); // 3_3
+
+    ctx.strokeStyle = "rgb(255,255,255,0.7)"; //枠の線
     ctx.beginPath();
     ctx.moveTo(180,0);
     ctx.lineTo(180,540);
@@ -50,9 +52,20 @@
     ctx.moveTo(0,360);
     ctx.lineTo(540,360);
     ctx.stroke();
-  };
     
-  image.src = "/assets/puzzle1.jpg";
-
+    
+  
 }
 
+document.addEventListener("DOMContentLoaded", function(event) {
+  new Vue({
+    el: '#task',
+    data: {
+      methods: {
+        bottom_right: function(){
+          bottomRight.style.display="none";
+        }
+      }
+    }
+  })
+})
